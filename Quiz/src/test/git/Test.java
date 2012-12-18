e537daf06b79d3010afb1eef61187eb47c3b11cc
student <student@172.16.12.129>
2012-12-18 12:42:29 +0100

    world > git

--- Quiz/src/test/git/Test.java
+++ Quiz/src/test/git/Test.java
@@ -3,7 +3,7 @@ package test.git;
 public class Test {
 	public static void main(String arg[])
 	{
-		System.out.println("Hello world");
+		System.out.println("Hello Git");
 		System.out.println("Hallo Roel");
 		System.out.println("Spawn more Overlords!");
 	}
