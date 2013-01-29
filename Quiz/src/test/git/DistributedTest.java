281296c9ab7e0bd56863dc2ff8091fb2d4d0bed9
student <student@172.16.12.129>
2013-01-29 10:55:32 +0100

    testing

--- Quiz/src/test/git/DistributedTest.java
+++ Quiz/src/test/git/DistributedTest.java
@@ -7,14 +7,14 @@ public class DistributedTest {
 	public static void main(String arg[])
 	{
 		ExecutorService ex = Executors.newCachedThreadPool();
-		Sender j = new Sender("127.0.0.1", 1337, "John");
-		Sender p = new Sender("127.0.0.1", 1337, "Paul");
+		//Sender j = new Sender("127.0.0.1", 1337, "John");
+		//Sender p = new Sender("127.0.0.1", 1337, "Paul");
 		Receiver server = new Receiver();
-		p.setSentence("You say goodbye and I say hello <done>");
+		//p.setSentence("You say goodbye and I say hello <done>");
 
 		ex.execute(server);
-		ex.execute(j);
-		ex.execute(p);
+		//ex.execute(j);
+		//ex.execute(p);
 		ex.shutdown();
 	}
 }
