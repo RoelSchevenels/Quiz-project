307b6cc3a6d8ab760b617074ea2e7b4ca318998c
student <student@172.16.12.129>
2013-01-29 10:55:04 +0100

    Worked on multiple choice panel

--- Quiz/src/GUI/player/MultipleChoiceAnswerPanel.java
+++ Quiz/src/GUI/player/MultipleChoiceAnswerPanel.java
@@ -1,6 +1,59 @@
 package GUI.player;
 
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
 public class MultipleChoiceAnswerPanel extends AnswerPanel
 {
-
+	private ArrayList<JButton> answerButtons;
+	
+	public static final int SINGLE_COLUMN = 0;
+	public static final int DOUBLE_COLUMN = 1;
+	public static final int TRIPPLE_COLUMN = 2;
+	public static final int QUADRUPLE_COLUMN = 3;
+	public static final int SINGLE_ROW = 4;
+	public static final int DOUBLE_ROW = 5;
+	public static final int TRIPPLE_ROW = 6;
+	public static final int QUADRUPLE_ROW = 7;
+	public static final int SQUARE = 8;
+	
+	
+	public MultipleChoiceAnswerPanel()
+	{
+		answerButtons = new ArrayList<JButton>();
+	}
+	
+	public MultipleChoiceAnswerPanel(ArrayList<String> answers)
+	{
+		answerButtons = new ArrayList<JButton>();
+		for (String answer : answers)
+		{
+			answerButtons.add(new JButton(answer));
+		}
+	}
+	
+	/**Draw the buttons
+	 *
+	 *
+	 * @param organization The way it gets organized
+	 */
+	private void drawButtons(int organization)
+	{
+		switch (organization)
+		{
+		case SINGLE_COLUMN:
+			setLayout(new GridLayout(answerButtons.size(), 1, 5, 5));
+			break;
+		case DOUBLE_COLUMN:
+			setLayout(new GridLayout(Math.round(answerButtons.size()/2.0f), 2, 5, 5));
+			break;
+		case TRIPPLE_COLUMN:
+			setLayout(new GridLayout(Math.round(answerButtons.size()/3.0f), 3, 5, 5));
+			break;
+		default:
+			break;
+		}
+	}
 }
