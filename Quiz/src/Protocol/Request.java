1059ebae96f292f98168752fec5759c609f245b2
Kristof Vrolijkx <kristofvrolijkx@gmail.com>
2013-02-24 11:12:54 +0100

    nikske

--- Quiz/src/Protocol/Request.java
+++ Quiz/src/Protocol/Request.java
@@ -3,6 +3,8 @@ package Protocol;
 import java.io.Serializable;
 
 public abstract class Request implements Serializable {
+	protected int RequestId;
+	
 	//
 	//alle request erven hier van over
 	//Gemeenschappelijke delen hier zetten
--- Quiz/src/Protocol/Response.java
+++ Quiz/src/Protocol/Response.java
@@ -3,6 +3,8 @@ package Protocol;
 import java.io.Serializable;
 
 public abstract class Response implements Serializable {
+	private int RequestId; //de id waarop de response een antwoord is
+	private int ResponseId;
 	//
 	//
 	//
