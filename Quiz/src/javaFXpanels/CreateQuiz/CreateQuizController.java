05e7e69b0b3e7fe553234faa69020ea9a86bf530
vncnt <vdmvinny@hotmail.com>
2013-02-28 19:41:58 +0100

    Vragen aanmaken GUI

---
+++ Quiz/src/javaFXpanels/CreateQuiz/CreateQuizController.java
@@ -0,0 +1,21 @@
+/**
+ * Controller klasse voor het aanmaken van quiz, vragen, rondes, etc..
+ * 
+ * @author De Meersman Vincent
+ */
+package javaFXpanels.CreateQuiz;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class CreateQuizController implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
