e344c474369eb221913135d6dffa660def335965
RoelSchevenels <roelschevenels@gmail.com>
2013-03-02 17:44:23 +0100

    Installer: Foutcontrole en properdere code

--- Quiz/installer-src/installer/Installer.java
+++ Quiz/installer-src/installer/Installer.java
@@ -7,32 +7,24 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  * @author Roel
  */
 public class Installer {
+	private static JFileChooser jfc;
 	public static void main(String arg[]) throws IOException
 	{
 		String regex = "<property name=\"hibernate.connection.url\">jdbc:h2:.*</property>";
 		String replacement = "<property name=\"hibernate.connection.url\">jdbc:h2:%s</property>";
 		String home = System.getProperty("user.home"); 
-		JFileChooser fc = new JFileChooser();
-		fc.setCurrentDirectory(new File(home));
-		String[] filetypes = {"zip", "jar"};
+		jfc = new JFileChooser();
+		jfc.setCurrentDirectory(new File(home));
 		
-		fc.setFileFilter(new FileNameExtensionFilter("Gezipte bestanden",filetypes));
-		fc.showOpenDialog(null);		
-		File zipfile = fc.getSelectedFile();
-		
-		fc.setSelectedFile(new File("Quiz.jar"));
-		fc.showSaveDialog(null);
-		File destination = fc.getSelectedFile();
-		
-		fc.setFileFilter(new FileNameExtensionFilter("Database", "db"));
-		fc.setSelectedFile(new File("Quiz"));
-		fc.showSaveDialog(null);
-		File db = fc.getSelectedFile();
+		File zipfile = getFile("", false,"Gezipte bestanden", "jar","zip");		
+		File destination = getFile("Quiz.jar",true,"Gezipte bestanden", "jar","zip");		
+		File db = getFile("Quiz", true, "Database", "db");
 		
 		replacement = String.format(replacement, db.getAbsolutePath());
 		
@@ -48,5 +40,30 @@ public class Installer {
 		
 		ZipUtil.updateZip(zipfile, destination, newcontent);
 		newcontent.delete();
+	}
+	
+	private static File getFile(String initialName, boolean save, String filterName, String... filter)
+	{
+		jfc.setFileFilter(new FileNameExtensionFilter(filterName, filter));
+		jfc.setSelectedFile(new File(initialName));
+		int res;
+		boolean exists = false;
+		while(!exists) {
+			if(save)
+				res = jfc.showSaveDialog(null);
+			else
+				res = jfc.showOpenDialog(null);
+			if(res == JFileChooser.APPROVE_OPTION) {
+				File file = jfc.getSelectedFile();
+				exists = file.exists();
+				if(exists || save)
+					return file;
+			} else {
+				JOptionPane.showMessageDialog(null, "Installatie geannuleerd");
+				System.exit(0);
+				return null;
+			}
+		}
+		return null;
 	}
 }
